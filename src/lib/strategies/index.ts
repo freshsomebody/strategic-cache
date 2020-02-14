56d29ec665a907d449db86571a341f62cbc0a956
Sam Liu <mailforailab@gmail.com>
2020-02-14 15:10:52 +0800

    feat: Export strategies

---
+++ src/lib/strategies/index.ts
@@ -0,0 +1,13 @@
+import StaleWhileRevalidate from './StaleWhileRevalidate'
+import CacheFirst from './CacheFirst'
+import FetchFirst from './FetchFirst'
+import FetchOnly from './FetchOnly'
+import CacheOnly from './CacheOnly'
+
+export {
+  StaleWhileRevalidate,
+  CacheFirst,
+  FetchFirst,
+  FetchOnly,
+  CacheOnly
+}
