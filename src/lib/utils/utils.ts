67adad90b5582b72e08285a6141154d4bf653938
Sam Liu <mailforailab@gmail.com>
2020-02-16 18:57:10 +0800

    feat: Verify cacheOptions.store

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -1,10 +1,39 @@
 export function cacheOptionValidator (overrideOptions: StrategicCache.CacheOptions = {}): StrategicCache.CacheOptions {
   const defaultOptions: StrategicCache.CacheOptions = {
+    store: 'memory',
+    fallbackStore: 'memory',
     maxAgeSeconds: 0,
     maxEntries: 0
   }
-
   const options = { ...defaultOptions, ...overrideOptions }
+
+  // Verify store
+  const { store } = options
+  const buildinStores = ['memory']
+  let storeErrorMsg = ''
+  switch (typeof store) {
+    case 'string':
+      storeErrorMsg = buildinStores.includes(store) ? '' : `${store} is not a supported store.`
+      break
+    case 'object':
+      storeErrorMsg = store.create ? '' : "Cannot find 'create' method in your store"
+      break
+    default:
+      storeErrorMsg = `${typeof store}' is not a supported store type.`
+  }
+  // If there is an error occurred in creating store
+  if (storeErrorMsg !== '') {
+    const { fallbackStore } = options
+    // If fallbackStore is disabled => throw Error
+    if (!fallbackStore) {
+      throw new TypeError(storeErrorMsg)
+    }
+    // Else => use fallbackStore
+    console.warn(storeErrorMsg)
+    console.warn(`Fall back to use '${fallbackStore}' store`)
+    options.store = fallbackStore
+  }
+
   // Verify ttlSeconds
   const { maxAgeSeconds } = options
   if (typeof maxAgeSeconds !== 'number' || maxAgeSeconds < 0) {
