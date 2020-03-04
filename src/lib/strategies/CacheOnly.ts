796f590878d5f394eb1001cfed739cf426ffc3f5
Sam Liu <mailforailab@gmail.com>
2020-03-04 13:52:37 +0800

    refactor: Move CacheOnly logics to cache.ts

--- src/lib/strategies/CacheOnly.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Get value from cache only
- * @see https://developers.google.com/web/tools/workbox/modules/workbox-strategies#cache_only
- * @param cacheStore cache store
- * @param key cache key
- */
-export default async function CacheOnly (cacheStore: StrategicCache.Store, key: string) {
-  return cacheStore.get(key)
-}
